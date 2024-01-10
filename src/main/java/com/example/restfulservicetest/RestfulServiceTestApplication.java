60e5ef6d3005f54520c79b848dfd713ece0f40e8
devgeniegenie <devgeniegenie@gmail.com>
2024-01-10 23:18:50 +0900

    init

---
+++ src/main/java/com/example/restfulservicetest/RestfulServiceTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulservicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulServiceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulservicetest/RestfulServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
