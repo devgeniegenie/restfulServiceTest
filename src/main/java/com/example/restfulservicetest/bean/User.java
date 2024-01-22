6ff72e2065bff6b371443da33de2ac1c1155003d
devgeniegenie <devgeniegenie@gmail.com>
2024-01-22 23:37:24 +0900

    - Spring Boot 3.xx, Spring Doc, Swagger - Spring Boot Actuator - HAL Explorer - 인증 정보를 갖고 있어야 API를 제공해 주기 위한 Spring Security 적용

--- src/main/java/com/example/restfulservicetest/bean/User.java
+++ src/main/java/com/example/restfulservicetest/bean/User.java
@@ -2,8 +2,9 @@ package com.example.restfulservicetest.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,18 +13,24 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"ssn", "password"})
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
+    @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성합니다.")
     private Integer id;
 
+    @Schema(title = "사용자 이름", description = "사용자 이름을 입력합니다.")
     @Size(min = 2, message = "Name은 2글자 이상")
     private String name;
 
+    @Schema(title = "사용자 등록자", description = "사용자 등록일을 입력합니다. 입력하지 않으면 현재 날짜가 지정됩니다.")
     @Past(message = "등록일은 미래 날짜 입력 불가능")
     private Date joinDate;
 
-//    @JsonIgnore
+    //@JsonIgnore
+    @Schema(title = "사용자 주민번호", description = "사용자의 주민번호를 입력합니다.")
     private String ssn;
 
-//    @JsonIgnore
+    //@JsonIgnore
+    @Schema(title = "사용자 비밀번호", description = "사용자의 비밀번호를 입력합니다.")
     private String password;
 }
--- src/main/java/com/example/restfulservicetest/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulservicetest/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,7 +2,7 @@ package com.example.restfulservicetest.exception;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-//import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -37,7 +37,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Map<String, Object> body = new HashMap<>();
 
         body.put("statusCode", HttpStatus.BAD_REQUEST);
